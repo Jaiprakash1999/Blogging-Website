ed2d72f8608626790dc957f87c2cf76924f99f1f
Jaiprakash kushwaha <jaiprakash.kushwaha@cogoport.com>
2022-06-10 18:44:26 +0530

    added some more features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 // import Reacts from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Topbar from './components/Home/Header/TopBar/Topbar';
 import Blog from './pages/Blog/Blog';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Write from './pages/Write/Write';
 
 const App = () => {
   return (
     <div className="container">
-      <h1 className='heading_style'> Blogger</h1>
+      <Topbar />
+     
       <Routes> 
         <Route path='/' exact element={<Home />}/>
         <Route path='/blog/:id' element={<Blog />}/>
+        <Route path='/Register' element={<Register />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Write' element={<Write />} />
       </Routes>
     </div>
   );
